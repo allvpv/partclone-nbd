3a186b9c8f4ad3699e28c4c8e785553753250ecd
Przeysław Kusiak <primo@poczta.fm>
2016-01-13 19:02:08 +0100

    State more clearly about authors and licese of crc.c file.

--- include/crc.h
+++ include/crc.h
@@ -1,13 +1,28 @@
+/* Copyright © 2015-2016 Przemysław Kusiak
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the “Software”), to
+ * deal in the Software without restriction, including without limitation the
+ * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+ * sell copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+ * IN THE SOFTWARE.
+ */
+
 #ifndef CHECKSUM_H_INCLUDED
 #define CHECKSUM_H_INCLUDED
 
 #include "partclone.h"
-
-#define CHECKSUM_NONE       0x00
-#define CHECKSUM_CRC32      0x20
-// Partclone images ver. 1 used defective CRC32 implementation. It doesn't
-// increment buffer -- only the first byte was computed over and over again
-#define CHECKSUM_DEFECTIVE  0xFF
 
 void initialize_crc32(void);
 u32 count_crc32(const void* data, size_t length, u32 previous_crc32);
